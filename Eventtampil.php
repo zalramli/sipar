dedacdfd4953dc7a98d1f51d214370f8f54ca39a
aliyek <aliwajhah1@gmail.com>
2018-11-14 21:16:59 +0700

    form event

---
+++ Eventtampil.php
@@ -0,0 +1,65 @@
+<!-- Breadcrumbs-->
+<ol class="breadcrumb">
+  <li class="breadcrumb-item">
+    <a href="index.html">Dashboard</a>
+  </li>
+  <li class="breadcrumb-item active">Tampil Event</li>
+</ol>
+<div class="card mb-3">
+            <div class="card-header">
+              <i class="fas fa-table"></i>
+              Data Event</div>
+            <div class="card-body">
+              <a href="?/=eventInput"><button class="btn btn-outline-success"><i class="fa fa-plus"> Tambah</i></button></a>
+              <div class="table-responsive"><br>
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    <tr style="text-align: center;">
+                      <th>No</th>
+                      <th>Nama</th>
+                      <th>Tanggal</th>
+                      <th>Lokasi</th>
+                      <th>gambar</th>
+                      <th>Deskripsi</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                    $no=1;
+                    $query = mysqli_query($koneksi,"SELECT * FROM event ");
+                    foreach ($query as $data ) {
+                     ?>
+                    
+                    <tr>
+                      <td width="2%"><?php echo $no++."." ?></td>
+                      <td width="10%"><?php echo $data['nama']; ?></td>
+                      <td width="10%"><?php echo $data['tgl_event']; ?></td>
+                      <td width="13%"><?php echo $data['lokasi']; ?></td>
+                      <td width="20%"><img width="245" height="150" src="asset/img/gprofil_wisata/<?php echo $data['gambar'] ?>" alt=""></td>
+                      <td width="25%"><?php echo substr($data['deskripsi'],0,40)." ..."; ?></td>
+                      <td style="text-align: center;" width="20%">
+                        <a class="btn btn-outline-secondary" href=""><i class="fa fa-eye"></i></a>
+                        <a href="?/=wisata_edit&id=<?php echo $data['id_wisata']; ?>" class="btn btn-outline-primary" href=""><i class="fa fa-edit"></i></a>
+                        <a style="margin-top: 5px;" class="btn btn-outline-danger" href=""><i class="fa fa-trash"></i></a>
+                      </td>
+                    </tr>
+                    <?php 
+                    } 
+                    ?> 
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>  
+
+<!--           <?php
+          $sql=mysqli_query($koneksi,"SELECT * FROM wisata WHERE id_wisata='1'");
+          $data=mysqli_fetch_array($sql);
+          $images=$data['detail_gambar'];
+          $temp = explode(',',$images);
+          for($i=0;$i<count($temp);$i++)
+          {
+            echo '<img style="margin:10px;" src="asset/img/gdetail_wisata/'.$temp[$i].'" height="160" width="210">&nbsp';
+          }
+          ?>
+ -->
---
+++ eventInput.php
@@ -0,0 +1,116 @@
+<?php
+if (isset($_POST['simpan'])) {
+  $nama2 = ucwords($_POST['nama2']); // awal kata huruf kapital
+  $tgl = $_POST['tgl'];
+  $lokasi1 = ucwords($_POST['lokasi1']);
+  $deskripsi1 = $_POST['deskripsi1'];
+  //gambar profil
+  $gbr = $_FILES['gbr']['name'];
+  $file_tmp = $_FILES['gbr']['tmp_name'];  
+  //gambar_detail
+
+  
+          $query = mysqli_query($koneksi,"INSERT INTO event (nama,tgl_event,lokasi,gambar,deskripsi) VALUES('$nama2','$tgl','$lokasi1','$gbr','$deskripsi1')");
+        if($query){
+        echo "<script>window.location = 'backend.php?/=Eventtampil'</script>";
+        }else{
+          echo 'Gagal Insert';
+        }
+
+  }
+  ?>
+
+<ol class="breadcrumb">
+  <li class="breadcrumb-item">
+    <a href="index.html">Dashboard</a>
+  </li>
+  <li class="breadcrumb-item active">Input Event</li>
+</ol>  
+<a href="?/=eventTampil"><button class="btn btn-success"><i class="fa fa-arrow-circle-left"> Kembali</i></button></a>
+
+<form action="" method="post" enctype="multipart/form-data"><br>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputText"><b>nama event</b></label>
+      <input type="text" name="nama2" class="form-control" id="inputText" placeholder="Masukan nama">
+    </div>
+    <div class="form-group col-md-6">
+         <label for="inputText"><b>Tanggal Event</b></label>
+      <input type="date" name="tgl" class="form-control">
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputText"><b>Lokasi</b></label>
+      <input type="text" name="lokasi1" class="form-control" id="inputText" placeholder="Masukan lokasi">
+    </div>
+    
+    <div class="form-group col-md-6">
+      <div class="imageupload">
+        <label for="inputText"><b>Gambar Profil</b></label>
+        
+        <div class="file-tab panel-body">
+          <div class="btn btn-warning btn-file">
+            <span>Browse</span>
+            <input type="file" name="gbr">
+          </div>
+          <button type="button" class="btn btn-danger">Hapus</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-12">
+      <label for="inputText"><b>Deskripsi</b></label>
+      <textarea class="form-control" name="deskripsi1" placeholder="Masukan deskripsi . . . . " id="exampleFormControlTextarea1" rows="7"></textarea>
+    </div>
+  </div>
+  
+  <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+</form><br>
+
+
+
+
+
+    <script src="backend/template/js/jquery-1.12.4.min.js"></script>
+    <script src="backend/template/js/bootstrap.min.js"></script>
+    <script src="backend/template/js/bootstrap-imageupload.js"></script>
+
+    <script>
+    var $imageupload = $('.imageupload');
+    $imageupload.imageupload();
+    </script>
+
+    <script language="javascript" type="text/javascript">
+    window.onload = function () {
+        var fileUpload = document.getElementById("fileupload");
+        fileUpload.onchange = function () {
+            if (typeof (FileReader) != "undefined") {
+                var dvPreview = document.getElementById("dvPreview");
+                dvPreview.innerHTML = "";
+                var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.jpg|.jpeg|.gif|.png|.bmp)$/;
+                for (var i = 0; i < fileUpload.files.length; i++) {
+                    var file = fileUpload.files[i];
+                    if (regex.test(file.name.toLowerCase())) {
+                        var reader = new FileReader();
+                        reader.onload = function (e) {
+                            var img = document.createElement("IMG");
+                            img.height = "150";
+                            img.width = "219";
+                            img.src = e.target.result;
+                            dvPreview.appendChild(img);
+                        }
+                        reader.readAsDataURL(file);
+                    } else {
+                        alert(file.name + " is not a valid image file.");
+                        dvPreview.innerHTML = "";
+                        return false;
+                    }
+                }
+            } else {
+                alert("This browser does not support HTML5 FileReader.");
+            }
+        }
+    };
+    </script>
